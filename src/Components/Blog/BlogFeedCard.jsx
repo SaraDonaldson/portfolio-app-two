9d69cc20a202c3a77a3b632ca763fc4c14d4250a
SaraDonaldson <115354783+SaraDonaldson@users.noreply.github.com>
2023-07-10 11:16:27 +0100

    basic styling for blog card reusable component

--- src/Components/Blog/BlogFeedCard.jsx
+++ src/Components/Blog/BlogFeedCard.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import { React, useState } from 'react';
+import './blogFeedCard.css';
 
 function BlogFeedCard({blogContent}) {
+    let [blogCardBody, setBlogCardBody] = useState("")
+
+    // function blogCardLimit(){
+
+
+    // }
+
+
+
   return (
     <div className='blog-feed-card'> 
-    
-        <h1 className='blog-content-title'>{ blogContent.title}</h1>
-       
 
+        <div className='blog-card-title-section'>
+        <h1 className='blog-card-title blog-card-white-space'>{ blogContent.title}</h1>
+        </div>
+
+
+        <div className='blog-card-body-container'>
+       
+            <div className='blog-card-image-container'>
+                <img className='blog-card-image'  src= {blogContent.mainImage} alt='' />
+            </div>
+            <h2 className= 'blog-card-subtitle blog-card-white-space'> {blogContent.subtitle}</h2>
+            <div className='blog-card-body'>
+                <p className='blog-card-intro-text blog-card-white-space'>
+                    {blogContent.paraOne}
+                </p>
+            </div>
+
+        </div>
 
     </div>
   )
--- src/Pages/BlogFeed.jsx
+++ src/Pages/BlogFeed.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import BlogFeedCard from '../Components/Blog/BlogFeedCard'
+import React from 'react';
+import BlogFeedCard from '../Components/Blog/BlogFeedCard';
+import './blogFeed.css';
+import codePenBall from '../Images/codePenBall.png'
 
 function BlogFeed() {
 
   let CssBoxShadows = {
-          title: "CSS: Art with a single element using only box shadows"
+          title: "CSS: Art with a single element using only box shadows",
+          subtitle: "How far can I go?",
+          mainimage: codePenBall,
+          mainimagealt: "preview of a clear pink ball made with CSS",
+          paraOne: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro earum, excepturi sed pariatur repellendus eos aperiam id eius officia veniam voluptatum iste, laborecorrupti aut quas voluptates voluptate. Ab, labore."
 
   }
 
@@ -12,13 +18,11 @@ function BlogFeed() {
   return (
     <div className='app-content blog-feed'>
       
-      
+      <div className='blog-feed-main-container'>
       <BlogFeedCard
       blogContent={CssBoxShadows}
       />
-
-
-
+      </div>
     </div>
   )
 }
